c7ca27a27ba36b670949a2bedc620c08c51110e7
Scott Purdy <scott@fer.io>
2015-01-20 10:12:20 -0800

    Remove NuPIC from example.

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 #include "inner.h"
 #include "outer.h"
 
-int main(int argc, const char* argv[]) {
+void testOuter() {
   string s("blah");
 
   example::Outer a(3.14159, 11, s);
@@ -31,4 +31,32 @@ int main(int argc, const char* argv[]) {
   assert(innerb.getF1() == 11);
   assert(innera.getF2() == s);
   assert(innerb.getF2() == s);
+}
+
+void testInner() {
+  string s("blah");
+
+  example::Inner a(11, s);
+
+  ofstream out("test.bin");
+  a.write(out);
+  out.close();
+
+  example::Inner b;
+  ifstream in("test.bin");
+  b.read(in);
+  in.close();
+
+  unlink("test.bin");
+
+  // assertions
+  assert(a.getF1() == 11);
+  assert(b.getF1() == 11);
+  assert(a.getF2() == s);
+  assert(b.getF2() == s);
+}
+
+int main(int argc, const char* argv[]) {
+  testOuter();
+  testInner();
 }
